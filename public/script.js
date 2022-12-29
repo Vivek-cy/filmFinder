8fcb302ec26a3e43ef6a665880947e3d40e67c04
Vivek Mandava <107625749+Vivek-cy@users.noreply.github.com>
2022-12-29 06:40:30 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -6,7 +6,6 @@ const getGenres = async () => {
   const genreRequestEndpoint = '/genre/movie/list';
   const requestParams = `?api_key=${tmdbKey}`;
   const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
-
   try{
     const response = await fetch(urlToFetch);
     if(response.ok){
@@ -14,7 +13,7 @@ const getGenres = async () => {
       const genres = jsonResponse.genres;
       return genres;
     }
-  } catch(error){
+  }catch(error){
     console.log(error);
   }
 };
@@ -22,9 +21,8 @@ const getGenres = async () => {
 const getMovies = async () => {
   const selectedGenre = getSelectedGenre();
   const discoverMovieEndpoint = '/discover/movie';
-  const requestParams = `?api_key=${tmdbKey}&with_genre=${selectedGenre}`;
+  const requestParams = `?api_key=${tmdbKey}&with_genres=${selectedGenre}`;
   const urlToFetch = `${tmdbBaseUrl}${discoverMovieEndpoint}${requestParams}`;
-  
   try{
     const response = await fetch(urlToFetch);
     if(response.ok){
@@ -37,17 +35,15 @@ const getMovies = async () => {
   }
 };
 
-
-const getMovieInfo = async movie => {
+const getMovieInfo = async (movie) => {
   const movieId = movie.id;
   const movieEndpoint = `/movie/${movieId}`;
-  const requestParams = `?api_key=${tmdbKey}`;
+  const requestParams = `?api_key=${tmdbKey}`
   const urlToFetch = `${tmdbBaseUrl}${movieEndpoint}${requestParams}`;
-
   try{
     const response = await fetch(urlToFetch);
     if(response.ok){
-      const movieInfo = response.json();
+      const movieInfo = await response.json();
       return movieInfo;
     }
   }catch(error){
@@ -61,10 +57,10 @@ const showRandomMovie = async () => {
   if (movieInfo.childNodes.length > 0) {
     clearCurrentMovie();
   };
-  const movies = await getMovies();
-  const randomMovie = getRandomMovie(movies);
-  const info = await getMovieInfo(randomMovie);
-  displayMovie(info);
+ const movies = await getMovies();
+ const randomMovie = getRandomMovie(movies);
+ const info = await getMovieInfo(randomMovie);
+ displayMovie(info);
 };
 
 getGenres().then(populateGenreDropdown);
